d78d45796e8d4b4a23a8b146ba6c12e2e12eadd5
sobadatcoding <christophernovellicain@outlook.com>
2024-05-06 19:48:44 +0100

    try n+1

--- sky-sphere.js
+++ sky-sphere.js
@@ -5761,7 +5761,7 @@ SkySphere = function (constellations) {
         }
     } 
 
-    context.font = this.options.font || '85px Helvetica';
+    context.font = this.options.font || '30px Helvetica';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
     context.fillStyle = '#ffffff';
